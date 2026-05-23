a50d1927bb77e98e9cb06995b0d42c9d2ef84c68
James Davis <6515965+The-Davis@users.noreply.github.com>
2026-05-23 19:57:54 -0400

    Update __init__.py

--- codex/__init__.py
+++ codex/__init__.py
@@ -8,6 +8,12 @@ sunk_cost_fallacy = CodexEntry(
 """,
 )
 
+druthi = CodexEntry(
+    title="Druthi",
+    content="""The Druthi are a race of humanoids with elvish features. They are known for their love of the dramatic, their emotional outbursts, and their reliably unreliable codes of personal honor.
+""",
+)
+
 ozkur = CodexEntry(
     title="Ozkur",
     content="""The Ozkur are a race of gray-skinned humanoids with some vaguely elvish and orcish features. They are known for their odd birth rates (70% female), their culinary kleptomania, and their rambunctious nature.
