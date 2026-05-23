9ae92547b33592831cfa7b8e73f647db48727196
James Davis <6515965+The-Davis@users.noreply.github.com>
2026-05-23 18:24:34 -0400

    Update __init__.py

--- codex/__init__.py
+++ codex/__init__.py
@@ -1,6 +1,25 @@
 from models.codex import CodexEntry
 
-_entries: list[CodexEntry] = []
+
+sunk_cost_fallacy = CodexEntry(
+    title="Sunk Cost Fallacy",
+    content="""The *Sunk Cost Fallacy* is a planejammer, a magical ship.
+
+""",
+)
+
+ozkur = CodexEntry(
+    title="Ozkur",
+    content="""The Ozkur are a race of gray-skinned humanoids with some vaguely elvish and orcish features. They are known for their odd birth rates (70% female), their culinary kleptomania, and their rambunctious nature.
+    Ozkuro (males) tend to keep herds of Ozkura (females), who themselves flit in and out of a male's care as they please. Most Ozkura settle down with their male after their first child is born, but some never do.
+""",
+)
+
+
+_entries: list[CodexEntry] = [
+    sunk_cost_fallacy,
+    ozkur,
+]
 
 
 def get_codex() -> list[CodexEntry]:
