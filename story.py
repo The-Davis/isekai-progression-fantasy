b871edd80fb8c354b15ff8f01d9b8085c6c6e3f9
James Davis <6515965+The-Davis@users.noreply.github.com>
2026-05-23 21:07:12 -0400

    Update story.py

--- story.py
+++ story.py
@@ -5,7 +5,7 @@ from models.story import Story
 current_story = Story(
     title="A Neophyte's Guide to the Planes",
     genre="Absurdist planar fantasy adventure story",
-    setting_notes="""The planes are real. Magic is real. The vastness of the cosmos is overwhelming. Humanity isn't invited, but they sure do make good passengers.
+    setting_notes="""This is essentially "Hitchhiker's Guide to the Galaxy", but set in Planescape or Spelljammer. The planes are real. Magic is real. The vastness of the cosmos is overwhelming. Humanity isn't invited, but they sure do make good passengers.
     Our main viewpoint character finds himself carried off on adventure, a la Arthur Dent (minus the exploding Earth. Earth's fine. Also, he's American, not British).
     Travelling on some sort of Spelljammer-like planar vessel between worlds, our hero has odd adventures. It is an action-oriented story, but with some absurdity akin to that of Douglas Adams.
 """,
