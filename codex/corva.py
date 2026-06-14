960effd3474020cf60c35727812ae11dc1076533
James Davis <6515965+The-Davis@users.noreply.github.com>
2026-06-14 17:50:33 -0400

    Update corva.py

--- codex/corva.py
+++ codex/corva.py
@@ -9,6 +9,7 @@ corvish_isles = CodexEntry(
     The Bannish Lowlands are industrious, boasting colossal factories and textile mills that rival Dornon's output. Its Highlands are a wild, romantic expanse of sweeping glens, deep, freezing lochs, and jagged, snow-capped mountains. Bannish Highlanders are fearsome, fiercely loyal infantry regiments who march to war to the drone of Bannish bagpipes.
     The mountainous western expanse of Corvland is Rhondda Taf, home to the Rhondish ethnolinguistic group. It is a land of deep, rain-swept valleys, jagged peaks, and ancient stone. Rhondda Taf is the heavy-mining capital of Corva; its mountains are dotted with colossal headframes where generations of Rhondish men have dug out iron, slate, and coal reserves. The Rhondish people are fiercely proud, culturally distinct, and highly musical.
     The island of Galidon is often called "The Emerald Isle" for its staggering, vibrant greenery. It is the most fertile of the Corvish Isles. Galidon is predominantly agricultural, dotted with ancient, ruined pre-Collapse monasteries and sprawling Canthican cathedrals. It produces the famous "Galish Wool" and provides much of the Empire's basic foodstuffs. Historically, Galidor had a fraught, rebellious relationship with the Crown, having endured several brutal pacifications in centuries past. Today, it is firmly under Crown rule, though resentments still simmer in its poorer, western counties, making it a volatile voting bloc in the House of Commons.
+The Corvish Isles are almost exclusively human. Non-humans are seldom welcome, but they can be found in ports. Inland, they're hunted as vagabonds.
 """
 )
 
@@ -20,7 +21,7 @@ dornon = CodexEntry(
     Most of these are ancient names that have been passed down for centuries, and the original meaning of the names has been lost to time.
     Many are run down and dangerous, but their denizens take fierce pride in their neighborhoods and are loyal to their own.
     Streets should have charming and interesting names as well, like Binders' Walk and the Red Row.
-    The city is a bustling metropolis, with a population of over 80 million people. It is a center of commerce, culture, and politics, and is home to the Corvish royal family.
+    The city is a bustling metropolis, with a population of over 8 million people. It is a center of commerce, culture, and politics, and is home to the Corvish royal family.
 """
 )
 
