9945a6229e1412811ebe7a2b08371786c725556a
James Davis <6515965+The-Davis@users.noreply.github.com>
2026-07-19 15:57:08 -0400

    Update __init__.py

--- codex/__init__.py
+++ codex/__init__.py
@@ -4,11 +4,15 @@ from codex.religion import religion_entries
 
 litrpg_elements = CodexEntry(
       title="LitRPG Elements of This Story",
-      content="""While we have RPG-style stats in this story, there is no "system", no quests, no achievements, or any other gamelike elements. Stats are more of a "natural philosophy" attempt to measure a person's abilities.
-There is magic, but not too many people can use it. Those who can are often able to objectively measure stats. The major attributes are physical, mental, and magical. Each attribute has five stats measuring the power, control, speed, capacity, and recovery of that attribute.
-The physical attributes are Might (Power), Finesse (Control), Agility (Speed), Endurance (Capacity), and Vigor (Recovery).
-The mental attributes are Intellect (Power), Focus (Control), Wit (Speed), Memory (Capacity), and Lucidity (Recovery).
-The magical attributes are Essence (Power), Dominion (Control), Alacrity (Speed), Vessel (Capacity), and Resonance (Recovery).
+      content="""While we have RPG-style stats in this story, there is no "system", no quests, no achievements, or any other gamelike elements. Stats are the mortals' attempts to measure a person's abilities.
+Stats can be enhanced through the collection and application of mana, which some people are able to channel into the body. Some do so through meditation, while others force it in with mana crystals.
+There are six attributes that most philosophies recognize:
+Strength is the body's physical power.
+Endurance is how long a body can exert itself and go without rest.
+Coordination is manual dexterity.
+Quickness is raw physical speed and reaction time.
+Focus is how well the mind focuses on a thought or a task.
+Willpower is a person's mental power, the "push" that forces magical action to take place, and the resistance against mental attack.
 """
 )
 
