05ee8f7b1927491df1b728068d16fe4afd1995b8
James Davis <6515965+The-Davis@users.noreply.github.com>
2026-07-19 16:29:59 -0400

    Update act_one.py

--- chapters/act_one.py
+++ chapters/act_one.py
@@ -4,11 +4,7 @@ from models.chapter import Chapter
 
 chapter_one = Chapter(
     title="Chapter One",
-    content="""If a man claims to know exactly how he managed to step straight out of the dark hollers of the Appalachian Mountains and into a world completely unacquainted with the great state of West Virginia, you can safely write him off as a liar. It happened to me, but I certainly couldn't tell you how. 
-One minute I was busting through a tangle of rhododendron in the dead of night, following the sweet, ringing music of my coonhound on a fresh trail, and the next, I was flat on my back in a field of purple heather under a sky boasting two moons.
-Right before everything changed, I'd got a feeling, a mighty peculiar one. It was this sudden, deep-bone tingle, kinda like the prickle that dances across your skin when a bolt of lightning strikes too close to the front porch. Only, this hum settled right into my marrow and stayed there, buzzing like a nest of angry hornets beneath my skin. I didn't understand it in the slightest. By and by, the shivering stopped, I opened my eyes, and that's when I realized I was flat on my back.
-I lay there a spell, trying to blink the double moons out of my vision, thinking I must've hit my head on a low-hanging branch, but the moons failed to come back together no matter how much I blinked. One was a pale, dusty yellow, not too far off from the one I'd known all my life, but the other glowed a sullen shade of bruised plum. It dawned on me right about then that the night air was uncommonly brisk against my skin. All of my skin.
-I sat up, took a quick inventory, and found myself entirely in the altogether. I was bereft of my boots, my denim trousers, my flannel shirt, and, most grievously, my hunting rifle. That last one was a bitter pill to swallow. It was my grandfather's Winchester, as fine a lever-action as a man could ever hope to carry into the woods. Losing your pants in the middle of the night is an embarrassment, but losing a piece of family history is a downright tragedy.
+    content="""
 """,
 )
 
