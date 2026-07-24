526ad341bcdb5a153c4ec675e1e78ba23ba8e4d8
James Davis <6515965+The-Davis@users.noreply.github.com>
2026-07-23 22:12:21 -0400

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,11 +1,7 @@
 import sys
 sys.dont_write_bytecode = True
-from story import current_story
 from chapters import get_all_chapters
 
-title = f" {current_story.title}" if current_story.title else ""
-
-print(f"Have been writing{title} for {current_story.work_time()}.")
 if get_all_chapters():
     for chapter in get_all_chapters():
         print(f"  {chapter.title}: {chapter.get_word_count()} words")
